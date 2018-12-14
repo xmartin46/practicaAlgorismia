6c68b051e6fa1983160214cb7df458a64d151ffe
chiquito <chiquito>
2018-12-14 23:51:16 +0100

    Nose

--- algorithms/Jsim.cpp
+++ algorithms/Jsim.cpp
@@ -16,10 +16,13 @@ int main() {
 	cout << "Insert the k value to do the k-Shingling: ";
 	cin >> k;
 	
-	string filePath1 = "./Jsim2documents/first.txt";
-	string filePath2 = "./Jsim2documents/second.txt";
+	//string filePath1 = "./Jsim2documents/first.txt";
+	//string filePath2 = "./Jsim2documents/second.txt";
 
-	bool spaces = false;
+	string filePath1 = "./20doc/13.txt";
+	string filePath2 = "./20doc/0.txt";
+
+	bool spaces = true;
 
 	unordered_set<string> S1 = kShingleString(filePath1, k, spaces);
 	unordered_set<string> S2 = kShingleString(filePath2, k, spaces);
