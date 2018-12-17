797c985fdd0f28d76c74cf58bf1680804fb7e63f
chiquito <chiquito>
2018-12-17 08:33:33 +0100

    ya vaaa

--- statsScripts/lsh_stats_k_t.py
+++ statsScripts/lsh_stats_k_t.py
@@ -4,17 +4,14 @@ martinPath = ''
 rogerPath = ''
 path = lacaPath
 
-file = open(path + '\\data\\lsh_time.csv', 'w')
+file = open(path + '\\data\\lsh_stats.csv', 'w')
 file.write('threshold, k, numCandidatePairs, trueCopyPairsDetected, falsePositivePairs, falseNegativePairs' + '\n')
 
 
 # n = number of hash functions applied to create signature matrix
-
+n = 500
 thres = [0.0, 0.2, 0.42, 0.64, 0.8, 1.0]
 for t in thres:
-    # nHashFunctions
-    n = 500
-
     # k = size of shingles
     for k in range(16):
         out = subprocess.check_output([path + '\\algorithms\\lsh.exe', str(k), str(0), str(1), str(n), str(t)])
