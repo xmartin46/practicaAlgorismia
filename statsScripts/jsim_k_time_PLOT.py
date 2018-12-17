b8ab178fa2cb422ff2a93076e0f3dded6931a5ef
xlacasa <xlcbdn@gmail.com>
2018-12-17 03:40:41 +0100

    meanTime_jsim

--- statsScripts/jsim_k_time_PLOT.py
+++ statsScripts/jsim_k_time_PLOT.py
@@ -11,18 +11,13 @@ path = lacaPath
 # Initialize the plot
 fig, ax = plt.subplots()
 
-# Read pairs
-pairsFile = open(path + '\\algorithms\\pairsOfDocsSelec.txt', 'r')
 
-for pairs in pairsFile.readlines():
-    # Deleteing the new line character at the end
-    pairs = pairs.rstrip('\n').split(" ")
-    # Importing the dataset
-    dt = pd.read_csv(path + '\\data\\jsim_time_' + str(pairs[0]) + '_' + str(pairs[1]) + '.csv')
-    # Plot the data
-    x = dt.iloc[:, 0]
-    y = dt.iloc[:, 1]
-    ax.plot(x, y, label='time for docs ' + str(pairs[0]) + ' and ' + str(pairs[1]), alpha=1.0)
+# Importing the dataset
+dt = pd.read_csv(path + '\\data\\jsim_meanTime.csv')
+# Plot the data
+x = dt.iloc[:, 0]
+y = dt.iloc[:, 1]
+ax.plot(x, y, alpha=1.0)
 
 # Add marker at average word size
 # ax.axvline(x=5.84, linestyle='--', linewidth=1, color='darkred', label='average word length (5.84)', alpha=0.75)
@@ -37,18 +32,16 @@ maxRange = int(np.max(x))+1
 plt.xticks([x * step for x in range(0, maxRange)])
 
 # Set "tick" frequency in y axis
-plt.yticks([x for x in range(0, 20)])
+plt.yticks([x for x in range(0, 15)])
 
 # Set titles
-ax.set(title="Average (of 5) Time to compute Jaccard Similarity", xlabel="shingle size (num chars)", ylabel="time (ms)")
+ax.set(title="Average (of 5) Time to compute Jaccard Similarity", xlabel="shingle size (num chars)", ylabel="mean time of 5 pairs of docs (ms)")
 
 # Show the plot
 # plt.show()
 
 # Save the plot
-plt.savefig(path + '\\plots\\jsim_k_time.png', bbox_inches='tight')
+plt.savefig(path + '\\plots\\jsim_k_meanTime.png', bbox_inches='tight')
 
 # Clear and close the plot
 plt.clf()
-
-pairsFile.close()
